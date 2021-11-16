791b3644f8a8f81fa543e47a7ff9dc5c1fed29e7
Brock Major <brockmajor@gmail.com>
2021-11-16 01:29:16 -0500

    simple savings and cc min unit testified

--- sandbox/flask_pytest.py
+++ sandbox/flask_pytest.py
@@ -23,3 +23,32 @@ def test_cc_payoff():
     resDict['Total Interest Paid'] = 20.07
     assert response.status_code == 200 and response.json() == json.dumps(resDict)
     print("Test to see if CC Payoff API is working properly and returns correct response")
+
+def test_simple_savings_calc():
+    dict2 = {
+                "Initial Deposit": 1000,
+                "Monthly Contribution": 100,
+                "Time Period": 1.5,
+                "Interest Rate": 10
+            }
+    response = requests.post(url+'/simple_savings_calc', json=dict2)
+    resDict = dict()
+    resDict['Total Savings balance'] = 3020.00   
+    resDict['Total Contributions'] = 1800.0
+    resDict['Interest Earned'] = 220.0
+    assert response.status_code == 200 and response.json() == json.dumps(resDict)
+    print("Test to see if Simple Savings Calc API is working properly and returns correct response")
+
+def test_cc_min_payment_calc():
+    dict2 = {
+                "CC Balance": 5000,
+                "CC Interest Rate": 6,
+                "Minimum Payment Percent": 9
+            }
+    response = requests.post(url+'/cc_min_payment_calc', json=dict2)
+    resDict = dict()
+    resDict['Monthly Payment'] = 450.00   
+    resDict['Months'] = 49
+    resDict['Total Payment'] = 5289.53
+    assert response.status_code == 200 and response.json() == json.dumps(resDict)
+    print("Test to see if CC Min Payment Calc API is working properly and returns correct response")
